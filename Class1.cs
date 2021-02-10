75be8031f3eb86d674e6f66be93f72424ac96c61
ortodox2002ortodox <58324913+ortodox2002ortodox@users.noreply.github.com>
2021-02-10 13:18:12 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Drawing.Imaging;
+
+namespace l.r._4
+{
+    class Task_2
+    {
+        public static void Task(string directory, string resultDirectory)
+        {
+            string[] fileName = Directory.GetFiles(directory).Select(Path.GetFileName).ToArray();
+            Regex regexExtForImage = new Regex("^((.bmp)|(.gif)|(.tiff?)|(.jpe?g)|(.png))$", RegexOptions.IgnoreCase);
+            int n = 0;
+            foreach (String i in fileName)
+            {
+                try
+                {
+                    if (regexExtForImage.IsMatch(Path.GetFileName(i)))
+                    {
+                        Bitmap bitmap = (Bitmap)Bitmap.FromFile(i);
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipX);
+                        bitmap.Save(resultDirectory + i.Split('.')[0] + "-mirrored.gif", ImageFormat.Png);
+                    }
+                }
+                catch (OutOfMemoryException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Картинка збережна в {0}.", resultDirectory);
+                    n++;
+                }
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace l.r._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] directiry = Directory.GetFiles(@"C:\textLaba4");
+            List<int> num = new List<int>();
+            int p = 0;
+            for (int i = 10; i < 30; i++)
+            {
+                string filename = i + ".txt"; 
+                string path = @"C:\textLaba4\" + i + ".txt";
+                FileInfo fileinfo = new FileInfo(path);
+                string[] array = fileinfo.Name.Split(".");
+                string prob = array[0];
+                try
+                {
+                    int[] arr = ReadAllInfo(path);
+                    int e = (TryFindProduct(arr, path));
+                    if(e!=0)
+                    {
+                        p += 1;
+                    }
+                    num.Add(e);
+                }
+                catch (Exception ex)
+                {
+                    using (StreamWriter sw = new StreamWriter(@"C:\textLaba4\no_file.txt", true, System.Text.Encoding.Default))
+                    {
+                        sw.Write(i + ".txt" + "\n");
+                        continue;
+                    }
+                }
+            }
+            int sum = 0;
+            foreach (int n in num)
+            {
+                try
+                {
+                    sum = checked(sum + n);
+                }
+                catch (Exception ex)
+                {
+                    sum = (int)sum;
+                }
+            }
+            Console.WriteLine("Сума всiх добуткiв - {0}", sum);
+            Console.WriteLine("Середнє арифметичне - {0}", sum / p);
+            /*
+             * Завдання 2
+            */
+            Task_2.Task(@"C:\textlaba3", @"C:\");
+        }
+        public static int[] ReadAllInfo(string directiry)
+        {
+            string[] inform = File.ReadAllLines(directiry);
+            int[] array = new int[2];
+
+            int len = inform.Length;
+            if (inform.Length > 2) // если в файле больше 2-х чисел
+            {
+                using (StreamWriter sw = new StreamWriter(@"C:\textLaba4\bad_data.txt", true, System.Text.Encoding.Default))
+                {
+                    sw.Write(directiry + "\n");
+                    return array;
+                } 
+            }
+            try
+            {
+                array[0] = int.Parse(inform[0]);
+                array[1] = int.Parse(inform[1]);
+            }
+            catch (Exception exp) // не удалось прочитать
+            {
+                using (StreamWriter sw = new StreamWriter(@"C:\textLaba4\bad_data.txt", true, System.Text.Encoding.Default))
+                {
+                    sw.Write(directiry + "\n");
+                }
+            }
+            return array;
+        } //чтение файлов тхт
+        public static int TryFindProduct(int[] array, string directiry)
+        {
+            int result = 0;
+            try
+            {
+                result = checked((int)(array[0] * array[1]));
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter sw = new StreamWriter(@"C:\textLaba4\overflow.txt", true, System.Text.Encoding.Default))
+                {
+                    sw.Write(directiry + "\n");
+                    return 0;
+                }
+            }
+
+            return (array[0]) * array[1];
+        } // добутки чисел
+
+
+    }
+}
+
